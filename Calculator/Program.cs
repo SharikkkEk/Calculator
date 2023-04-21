29cb0c569e5432e899ab5ef67833917a7fc20638
Sharikekkk <skvorcovi662@gmail.com>
2023-04-21 21:19:17 +0400

    Начало

---
+++ Calculator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                int numberOne = 0;
+                int numberTwo = 0;
+
+                Console.WriteLine("Добро пожаловать в самый хреновый калькулятор!\nВведите первое число");
+
+                try
+                {
+                    numberOne = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Ты чё творишь, це не число\nЗапуск ядерных ракет");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.WriteLine("Введите второе число");
+                try
+                {
+                    numberTwo = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Ты чё творишь, це не число\nЗапуск ядерных ракет");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Main();
+                }
+
+                Console.WriteLine("Введите символ операции");
+                string symbolOperation = Console.ReadLine();
+                switch (symbolOperation)
+                {
+                    case "^":
+                        Console.WriteLine(numberOne ^ numberTwo);
+                        break;
+                    case ":":
+                        Console.WriteLine(numberOne / numberTwo);
+                        break;
+                    case "*":
+                        Console.WriteLine(numberOne * numberTwo);
+                        break;
+                    case "-":
+                        Console.WriteLine(numberOne - numberTwo);
+                        break;
+                    case "+":
+                        Console.WriteLine(numberOne + numberTwo);
+                        break;
+                    default:
+                        Console.WriteLine("Я не знаю такую операцию(((");
+                        break;
+                }
+                Console.WriteLine("\nввести цифру '1' - закончить выполнение программы\nвсё остальное - продолжить");
+                string cont = Console.ReadLine();
+                if (cont == "1")
+                {
+                    Console.WriteLine("Оцените калькулятор:");
+                    string score = Console.ReadLine();
+                    Console.WriteLine("Ваша оценка очень важна для нас");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number numberOne = new Number();
+
+            numberOne.numberByOrder = 1;
+            numberOne.count = numberOne.entry();
+
+            Console.ReadLine();
+        }
+    }
+    
+    public class Number
+    {
+        public int count;
+
+        public int numberByOrder;
+
+        public int entry()
+        {
+            Console.WriteLine($"Введите {numberByOrder} число");
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out count) == false)
+                {
+                    Console.WriteLine("Введите число заново");
+                    continue;
+                }
+                else
+                {
+                    return count;
+                }
+            }
+        }
+    }
+}
